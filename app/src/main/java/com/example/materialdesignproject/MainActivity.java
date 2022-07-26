0702724a05f30f51b9ccad2116295627fbb8181f
Sarina Hemmat pour <hemmatpour.sarina@gmail.com>
2022-07-26 11:45:05 +0430

    add basic components

--- app/src/main/java/com/example/materialdesignproject/MainActivity.java
+++ app/src/main/java/com/example/materialdesignproject/MainActivity.java
@@ -10,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* visit =>
+        https://guides.codepath.com/android/using-the-app-toolbar#:~:text=The%20key%20differences%20that%20distinguish,defined%20within%20a%20single%20activity
+        for toolbar and actionbar
+         */
+
     }
 }
