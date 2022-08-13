6b5f52ab1cca40897ff4b520df9ed89d9812532b
Sarina Hemmat pour <hemmatpour.sarina@gmail.com>
2022-08-13 22:47:11 +0430

    add drawer toggle and navigationView

--- app/src/main/java/com/example/materialdesignproject/MainFragment.java
+++ app/src/main/java/com/example/materialdesignproject/MainFragment.java
@@ -2,21 +2,26 @@ package com.example.materialdesignproject;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.SurfaceControl;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -57,8 +62,42 @@ public class MainFragment extends Fragment {
         /*toye fragment ha be getSupportActionBar() dastresi nadarim
         bayad ye instance az AppCompatActivity dashte bashim :
          */
+        Toolbar toolbar=(Toolbar) getView().findViewById(R.id.toolbarMain);
+
         AppCompatActivity appCompatActivity=(AppCompatActivity)getActivity();
-        appCompatActivity.setSupportActionBar((Toolbar) getView().findViewById(R.id.toolbarMain));
+        appCompatActivity.setSupportActionBar(toolbar);
+
+        //baraye drawer toggle e navigation view :
+        toolbar.setNavigationIcon(R.drawable.ic_baseline_menu_24);
+
+        DrawerLayout drawerLayoutMain=getView().findViewById(R.id.drawerLayout_main);
+        ActionBarDrawerToggle actionBarDrawerToggle=new ActionBarDrawerToggle(
+                getActivity(),drawerLayoutMain , toolbar,
+                R.string.openDrawer , R.string.closeDrawer
+        );
+        drawerLayoutMain.addDrawerListener(actionBarDrawerToggle);
+
+        NavigationView navigationView=getView().findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                //inja ham mishe switch gozasht o ....
+                return true;
+            }
+        });
+        //baraye dastresi be header
+        /* baraye navigtion view mishe tu code chandin header add kard
+        (na tu xml)
+        inja ham index headeri ke mikhaym ro behesh midim
+         */
+        View headerView=navigationView.getHeaderView(0);
+        TextView textView=headerView.findViewById(R.id.textView);
+        textView.setText(R.string.app_name);
+
+
+
+
+        // ////////////////////////////////////////////////////////////////////////////////////////
 
 
         MaterialCardView materialCardView=getView().findViewById(R.id.materialCardView_item);
